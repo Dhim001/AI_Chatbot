bec4061ddaa5f8c9340c85414f6d1a3017238894
Dimeji Rufai <rfdimeji@gmail.com>
2023-08-20 17:07:12 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:4000', {
+    const response = await fetch('https://ai-chatbot-s79m.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
